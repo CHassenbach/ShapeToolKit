985411032c981d29e45f6b1ece3ea268175b7269
rianbreak <florianbraig@t-online.de>
2026-06-09 11:05:05 +0300

    Changed overlay valeus for PC contrib plot, because +/- 2sd can show very extreme forms that might not appear in the data

--- R/pc_contribution_plots_module.R
+++ R/pc_contribution_plots_module.R
@@ -468,8 +468,11 @@ pc_contribution_plots_server <- function(id) {
     }
 
     # --- Overlay cell: -2SD (blue) vs +2SD (red) ---
-    coords_neg2 <- pc_shapes[["sd-2"]]
-    coords_pos2 <- pc_shapes[["sd2"]]
+    coords_neg2 <- pc_shapes[["sd-1"]]
+    coords_pos2 <- pc_shapes[["sd1"]]
+    # Note: using -1 SD shape for better visibility of
+    # differences, as -2 SD can be very extreme
+    #test
 
     graphics::par(mar = c(0.5, 0.5, 1.8, 0.5), bg = "white")
 
